b952e285cdee19d1461b186f1107fd68417e2741
Pluck Yang <pluck.yang@gmail..com>
2023-11-09 23:04:24 +0900

    update app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     // <HTMLTemplateElement>で document.getElementById から取得したelement種類を定義
@@ -12,7 +15,24 @@ class ProjectInput {
     // htmlファイル内の<template>のreferenceを取得
     const importedNode = document.importNode(this.templateElement.content, true);
     this.element = <HTMLFormElement>importedNode.firstElementChild;
-    this.attach;
+    this.element.id = 'user-input';
+
+    this.titleInputElement = <HTMLInputElement>this.element.querySelector('#title');
+    this.descriptionInputElement = <HTMLInputElement>this.element.querySelector('#description');
+    this.peopleInputElement = <HTMLInputElement>this.element.querySelector('#people');
+
+    this.configure();
+    this.attach();
+  }
+
+  private submitHandler(event: Event) {
+    // デフォルトのHTML requestを避ける
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler);
   }
 
   private attach() {
