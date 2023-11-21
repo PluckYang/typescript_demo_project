34c45b31975a052e2403b447d92803429ed3c3bf
Pluck Yang <pluck.yang@gmail..com>
2023-11-21 11:13:11 +0900

    rendering ProjectItem with a class

--- src/app.ts
+++ src/app.ts
@@ -157,6 +157,36 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
   abstract renderContent():void;
 }
 
+// ProjectItem class
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+  private project: Project;
+  
+  get persons() {
+    if(this.project.people === 1) {
+      return "1 person";
+    } else {
+      return `${this.project.people} persons`;
+    }
+  }
+
+  constructor(hostID: string, project: Project) {
+    super('single-project', hostID, false, project.id);
+    this.project = project;
+
+    this.configure();
+    this.renderContent();
+  }
+
+  configure() {}
+
+  renderContent() {
+    this.element.querySelector('h2')!.textContent = this.project.title;
+    this.element.querySelector('h3')!.textContent = this.persons + " assigned"; //this.personsは直接getterを使用、（）をつけるのは不要
+    this.element.querySelector('p')!.textContent = this.project.discription;
+  }
+}
+
+
 // ProjectList Class
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
   assignedProjects: Project[];
@@ -193,9 +223,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     const listEl = document.getElementById(`${this.type}-project-list`) as HTMLUListElement;
     listEl.innerHTML = '';
     for (const prjItem of this.assignedProjects) {
-      const listItem = document.createElement('li');
-      listItem.textContent = prjItem.title;
-      listEl.appendChild(listItem);
+      new ProjectItem(this.element.querySelector('ul')!.id, prjItem)
     }
   }
 }
