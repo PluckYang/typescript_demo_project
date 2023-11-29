f50b7b0f7fa3d458fad9929cec3d436601cbe82e
Pluck Yang <pluck.yang@gmail..com>
2023-11-29 19:47:40 +0900

    organizing files and folders

---
+++ src/decorators/autobind.ts
@@ -0,0 +1,21 @@
+namespace App {
+  // autobind decorator
+  export function autobind(
+    //"_"で定義されたが、使われてい無いparameterを表示
+    _: any, 
+    _2: string, 
+    descriptor: PropertyDescriptor
+  ) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+      configurable: true,
+      get () {
+        const boundFn = originalMethod.bind(this);
+        return boundFn;
+      }
+    };
+    return adjDescriptor;
+  }
+
+}
+
